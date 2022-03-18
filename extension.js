b4dcb3fb5532fb3577dfd4c56d09c2aeaa4ac2a1
shayankamandi <shayankamandi82@gmail.com>
2022-03-18 15:23:42 +0330

    some updates in panel

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,49 +9,69 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log("Congratulations, your extension Zap is now active!");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension Zap is now active!');
+  // vscode.window.createTreeView('ftpExplorer', {
+  //   treeDataProvider: new FtpTreeDataProviderea(),
+  // });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extention.zap', function () {
-		
-		vscode.window.showInformationMessage('Zap welcome to you!');
-		
-		context.subscriptions.push(vscode.commands.registerCommand('zap.panel.registration', () => {
-			return {
-				id: 'zap',
-				title: 'ZAP actions',
-				description: 'Actions for testing purposes only.',
-				actions: [{
-					title: 'Click me!',
-					command: 'vscode-extension-panel.panel.test',
-					data: true,
-					type: "checkbox"
-				}, {
-					title: 'Action 2',
-					command: 'vscode-extension-panel.panel.test',
-					data: 'Just extra text',
-					type: "button"
-				}]
-			};
-		})
-	);
-	});
-	
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the c  ommand with  registerCommand
+  // The commandId parameter must match the command field in package.json
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "shayankamandi.extention.panel.registration",
+      () => {
+        return {
+          id: "extention",
+          title: "Zap",
+          description:
+            "ZAP is Zigbee Cluster Library configuration tool and generator. It allows users to configure their ZCL application using web-like interface and then generate the required artifacts for this application, based upon the templates inside a given ZCL SDK.",
+          actions: [
+            {
+              title: "Binary Path",
+              command: "vscode-binary-path",
+              data: "Just Some text",
+              placeHolder: "binary...",
+              type: "text",
+            },
+            {
+              title: "Enable",
+              command: "vscode-extension-panel.panel.test",
+              data: true,
+              type: "checkbox",
+            },
+
+            {
+              title: "Run",
+              command: "zap-run-binary",
+              data: "Just Binary",
+              type: "button",
+            },
+          ],
+        };
+      }
+    )
+  );
+
+  let disposable = vscode.commands.registerCommand(
+    "extention.zap",
+    function () {
+      vscode.window.showInformationMessage("Zap welcome to you!");
+      vscode.window.setStatusBarMessage("Zap");
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
-
-
+  activate,
+  deactivate,
+};
