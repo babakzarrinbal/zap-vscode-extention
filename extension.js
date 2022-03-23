ab91ac6ff6a0f1be26256ced12bf035ff0c165f7
shayankamandi <shayankamandi82@gmail.com>
2022-03-23 19:03:59 +0430

    update panel

--- extension.js
+++ extension.js
@@ -13,57 +13,23 @@ function activate(context) {
   // This line of code will only be executed once when your extension is activated
   console.log("Congratulations, your extension Zap is now active!");
 
-  // vscode.window.createTreeView('ftpExplorer', {
-  //   treeDataProvider: new FtpTreeDataProviderea(),
-  // });
+  
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the c  ommand with  registerCommand
   // The commandId parameter must match the command field in package.json
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand(
-      "shayankamandi.extention.panel.registration",
-      () => {
-        return {
-          id: "extention",
-          title: "Zap",
-          description:
-            "ZAP is Zigbee Cluster Library configuration tool and generator. It allows users to configure their ZCL application using web-like interface and then generate the required artifacts for this application, based upon the templates inside a given ZCL SDK.",
-          actions: [
-            {
-              title: "Binary Path",
-              command: "vscode-binary-path",
-              data: "Just Some text",
-              placeHolder: "binary...",
-              type: "text",
-            },
-            {
-              title: "Enable",
-              command: "vscode-extension-panel.panel.test",
-              data: true,
-              type: "checkbox",
-            },
-
-            {
-              title: "Run",
-              command: "zap-run-binary",
-              data: "Just Binary",
-              type: "button",
-            },
-          ],
-        };
-      }
-    )
-  );
-
+  
   let disposable = vscode.commands.registerCommand(
     "extention.zap",
     function () {
       vscode.window.showInformationMessage("Zap welcome to you!");
       vscode.window.setStatusBarMessage("Zap");
+      
     }
   );
+
+  
 
   context.subscriptions.push(disposable);
 }
