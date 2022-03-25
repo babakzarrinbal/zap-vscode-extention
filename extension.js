78806cbcb803b372c0082276a4ec3887a726ecc8
Babak Zarrinbal <babak.zarrinbal@gmail.com>
2022-03-25 18:38:16 +0430

    applying changes

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 var path = require("path");
-
+const { exec } = require("child_process");
 // const btn = json.contributes.viewsWelcome(view: string; context:"\n[Open File](command:workbench.action.files.openLocalFile)\n" );
 
 /**
@@ -16,38 +16,55 @@ function activate(context) {
         .getConfiguration("zap")
         .get("packageJsonFilePath");
       path = path.replaceAll("\\", "/");
-      console.log(path);
-      if (path.split("/").pop().toLowerCase() !== "package.json") {
-        path = path.slice(-1) === "/" ? path : path + "/";
-        path = path + "/package.json";
+      if (path.split("/").pop().toLowerCase() === "package.json") {
+        path = path.split("/")
+        path.pop()
+        path.join("/");
       }
+      path = path.slice(-1) === "/" ? path.slice(0,-1) : path ;
       let json;
       try {
-        json = require(path);
+        json = require(path+"/package.json");
       } catch (e) {
         return vscode.window.showInformationMessage("can't load package.json");
       }
-      vscode.window.showInformationMessage("adding commands: ", Object.keys(json.scripts).join(", "));
+      let items=  [];
+  
+      for (let key of Object.keys(json.scripts)){
+        items.push({ 
+          label: key, 
+          description: key});
+      }
+      
+      vscode.window.showQuickPick(items).then(selection => {
+        // the user canceled the selection
+        vscode.window.showInformationMessage(
+          "running commands: ",
+          selection.label
+        );
+        let channel = vscode.window.createOutputChannel(selection.label);
+        let a = exec('pwd', {
+          cwd: path
+        }, function(error, stdout, stderr) {
+          
+          channel.appendLine(error.toString())
+        });
+        a.on('message',m=>channel.append(m.toString()))
+      });
+
     }
   );
 
   let disposable = vscode.commands.registerCommand(
     "extention.zap",
-    function () {
-      var currentlyOpenTabfilePath =
-        vscode.window.activeTextEditor.document.uri.fsPath;
-      var currentlyOpenTabfileName = path.basename(currentlyOpenTabfilePath);
-      console.log(currentlyOpenTabfileName);
-
-      // const pathh = json.contributes.viewsWelcome;
-      // const a = vscode.commands.executeCommand("markdown.showPreview", vscode.Uri.file(pathh));
-      // console.log(a);
+    function (script) {
+      vscode.window.showInformationMessage("adding commands: ", script);
     }
   );
 
   // context.subscriptions.push(disposable,);
-  context.subscriptions.push(getconfig,);
-  context.subscriptions.push(disposable,);
+  context.subscriptions.push(getconfig);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
